e6d235f33321778fa8a3a2fdef45205dbd2ca555
tiararistaw <tiaraaristawidya15@gmail.com>
2025-11-05 22:21:41 +0700

    Update dailyreflection.php

--- dailyreflection.php
+++ dailyreflection.php
@@ -18,9 +18,9 @@ $nama_user = $user['nama_pengguna'] ?? 'Pengguna';
   <title>Dashboard Mahasiswa - Daily Reflection</title>
 
   <!-- Bootstrap & Font Awesome -->
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
-  <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+    <link rel="stylesheet" href="../assets/css/bootstrap.min.css"> 
+    <link rel="stylesheet" href="../assets/css/style.css">
 </head>
 
 <body style="background-color: #f2f2f2;">
